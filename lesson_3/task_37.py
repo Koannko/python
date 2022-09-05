5aeab55e8bdd0f62680a363dde0c30d25bc2c0ac
Koannko <kostyleva-2000@bk.ru>
2022-09-05 18:52:41 +0400

    init: task 37

--- lesson_3/task_37.py
+++ lesson_3/task_37.py
@@ -1,50 +1,37 @@
-import numpy
-
-facts = []
-
-
-def fact(N):
-    print(N)
-    if N == 0 or N == 1:
-        return 1
-    # if facts[N]:
-    #     return facts[N]
-    result = N * fact(N - 1)
-    return result
+# 37. Дан список чисел. Создать список в который попадают числа, описывающие
+# возрастающую последовательность и содержащие максимальное количество элементов.
+def subsets(arr, s_len):
+    subsets = [[]]
+    arr = set(arr)
+    for a in arr:
+        subsets += [s + [a] for s in subsets if len(s) < s_len]
+    return subsets
 
 
-def C(n, k):
-    return fact(n) / fact(k) / fact(n - k)
-
-
-def combination(index, k, A):
-
-    a = np.zeros(8)
-    print(a)
-
-    res = [0]
-    n = len(A)
-    s = 0
-    for t in range(1, k):
-        j = res[t - 1] + 1
-        while (j < (n - k + t)) and ((s + C(n - j, k - t)) <= index):
-            s += C(n - j, k - t)
+def find_subset(arr, subset, s_len):
+    count = 0
+    j = 0
+    for i in range(len(arr)):
+        if arr[i] == subset[j]:
+            count += 1
             j += 1
-            print(res)
-    res.append(j)
-
-    # res.insert(0, 1)
-
-    return res
+    return count == s_len
 
 
-print(combination(0, 3, [6, 7, 8, 9]))
+def get_largest_increas_seq(arr):
+    s_len = len(arr)
+    sets = subsets(arr, s_len)
+    while s_len:
+        S = list(filter(lambda s: len(s) == s_len, sets))
+        for s in S:
+            if find_subset(arr, s, s_len):
+                return s
+        s_len -= 1
 
-# def log():
-#     msg = Array.prototype.slice.call(arguments).join(" ")
-#     document.getElementById("log").value += "\n" + msg
-#     console.log(arguments)
 
-# M = ["A", "B", "C", "D", "E"];
-# for i in range(0, C(M.length, 3)):
-#     log(i, combination(i, 3, M.slice(0)).join(""))
+# => [0, 1, 2, 3, 4, 6, 7, 8, 9, 10]
+print(get_largest_increas_seq([0, 5, 1, 2, 2, 3, 4, 6, 7, 8, 9, 10]))
+# => [1, 2, 3, 4, 6, 7]
+print(get_largest_increas_seq([1, 5, 2, 3, 4, 6, 1, 7]))
+print(get_largest_increas_seq([5, 2, 3, 4, 6, 1, 7]))  # => [2, 3, 4, 6, 7]
+print(get_largest_increas_seq([1, 3, 5, 6, 7]))  # => [1, 3, 4, 5, 7]
